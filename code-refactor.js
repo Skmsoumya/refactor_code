736fe924959fe404c7d006d9b9ffd8fb311a8c11
Skmsoumya <soumyakishormishra@gmail.com>
2017-11-20 12:42:39 +0530

    remove the duplication of dates check code by extracting it to a new function

--- code-refactor.js
+++ code-refactor.js
@@ -1,21 +1,71 @@
-var showMessage = function(message_code) {
-	var messages = {
-		"dates_not_selected": "Please select your check-in and check-out dates",
-		"dates_in_past": "Please check-in date cannot be in past",
-		"no_stay_selected": "Please select atleast one stay/experience",
-		"booking_closed_for_today": "Booking is closed for today. Please try for next day.",
-		"internal_server_error": "Internal error, please refresh the page and continue"
-	};
-
-	$scope.showMessage(messages[message_code]);
+/*
+	Define all the messages at a single point
+*/
+var messages = {
+	"dates_not_selected": "Please select your check-in and check-out dates",
+	"dates_in_past": "Please check-in date cannot be in past",
+	"no_stay_selected": "Please select atleast one stay/experience",
+	"booking_closed_for_today": "Booking is closed for today. Please try for next day.",
+	"internal_server_error": "Internal error, please refresh the page and continue"
 };
+
+/*
+	Show a message. 
+	parameters :{
+		message_code: "String", The key name of the message to show.
+	}
+*/
+function showMessage(message_code) {
+	$scope.showMessage(messages[message_code]);
+}
+
+
+function showStartupVideo() {
+	if ($scope.listing.code === 'startuptour' && $scope.listing.config.default_date) {
+		angular.forEach($scope.requestedListingData.bookables, function (bookable) {
+			bookable.requested = 1;
+		});
+		return true;
+	}
+
+	return false;
+}
+
+
+
+/*
+	check if the no dates selected or the selected dates are wrong
+*/
+
+function datesCheck(pakageType, isPastCheckInDate) {
+	var datesFromCheck = !$scope.requestedListingData.date_from;
+	var datesUntillCheck = !$scope.requestedListingData.date_until;
+
+	var datesNotPresentCheck = pakageType === 1 ? ( 	datesFromCheck || 
+											datesUntillCheck ) :
+											datesFromCheck ;
+
+	if (datesNotPresentCheck) {
+		showMessage("dates_not_selected");
+		return false;
+	}
+	
+	var startupVideoCheck =  pakageType === 1 ? showStartupVideo() : false;
+	var pastDateCheckPass = isPastCheckInDate ? startupVideoCheck : true;
+
+	if (!pastDateCheckPass) {
+		showMessage("dates_in_past");
+	}
+	return pastDateCheckPass;
+}
+
+
+
 
 
 $scope.checkAllFieldsPresent = function () {
 
-	/*
-		Check if requested listing data present
-	*/
+	
 	if ($scope.requestedListingData) {
 
 
@@ -32,35 +82,9 @@ $scope.checkAllFieldsPresent = function () {
 				atleastOneBookableSelected = true;
 			}
 		});
-
-		if ($scope.packeageType === 1) {
-			if (!$scope.requestedListingData.date_from || !$scope.requestedListingData.date_until) {
-				showMessage("dates_not_selected");				
-				return false;
-			} else {
-				if (isPastCheckInDate) {
-					showMessage("dates_in_past");
-					return false;
-				}
-			}
-		} else {
-			if (!$scope.requestedListingData.date_from) {
-				showMessage("dates_not_selected");
-				return false;
-			} else {
-				// hack to get statup tour request video working
-				if (isPastCheckInDate) {
-					if ($scope.listing.code === 'startuptour' && $scope.listing.config.default_date) {
-						angular.forEach($scope.requestedListingData.bookables, function (bookable) {
-							bookable.requested = 1;
-						});
-						return true;
-					}
-
-					showMessage("dates_in_past");
-					return false;
-				}
-			}
+		var datesCheckVal = datesCheck($scope.packeageType, isPastCheckInDate);
+		if(!datesCheckVal) {
+			return datesCheckVal;
 		}
 
 		if (!atleastOneBookableSelected) {
